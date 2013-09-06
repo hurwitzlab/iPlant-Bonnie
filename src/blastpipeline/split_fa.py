a35a749d0c8b303680dfb3a5a7286d1d13097aeb
Darren Boss <darren.boss@gmail.com>
2013-09-06 11:31:58 -0400

    Added pydoc comments and main method

--- src/blastpipeline/split_fa.py
+++ src/blastpipeline/split_fa.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+# split_fa.py
+
+"""
+Split a directory full of fasta files into files with equal numbers of
+sequences per file
+"""
 
 import argparse
 import os
@@ -6,7 +12,10 @@ from Bio import SeqIO
 import glob
 
 
-def split_fasta(infile, files_wanted, total_sequences, file_counter):
+def split_fasta(infile, outdir, files_wanted, total_sequences, file_counter):
+    """
+    Split fasta file into x number of files
+    """
     seq_per_file = (total_sequences / files_wanted) + 1
     current_seq_count = 0
     total_seq_count = 0
@@ -24,35 +33,35 @@ def split_fasta(infile, files_wanted, total_sequences, file_counter):
             outfile = open(outdir + '/query' + str(file_counter), 'w')
     return file_counter
 
-parser = argparse.ArgumentParser(description='Split fasta file into \
-                                              smaller pieces')
-parser.add_argument('indir', help='Input directory')
-parser.add_argument('outdir', help='Output directory')
-parser.add_argument('-j', type=int, help='Number of jobs')
-args = parser.parse_args()
-indir = args.indir
-outdir = args.outdir
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
-if args.j:
-    jobs = args.j
-else:
-    jobs = 500
-orig_dir = os.getcwd()
-file_counter = 1
-for f in glob.glob(indir + '/*.fa'):
-    fasta = open(outdir + '/all_seqs', 'w+')
-    total_sequences = 0
-    infile = open(f, 'rU')
-    for line in infile:
-        if line.startswith('>'):
-            total_sequences += 1
-        fasta.write(line)
-    print total_sequences
-    fasta.seek(0)
-    file_counter = split_fasta(fasta, jobs, total_sequences, file_counter)
-    print file_counter
-    fasta.close()
-    os.remove(outdir + '/all_seqs')
-    infile.close()
-print "All done."
+
+def main():
+    parser = argparse.ArgumentParser(description='Split fasta file into \
+                                                  smaller pieces')
+    parser.add_argument('indir', help='Input directory')
+    parser.add_argument('outdir', help='Output directory')
+    parser.add_argument('-j', type=int, help='Number of jobs')
+    args = parser.parse_args()
+    indir = args.indir
+    outdir = args.outdir
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    if args.j:
+        jobs = args.j
+    else:
+        jobs = 500
+    file_counter = 1
+    for f in glob.glob(indir + '/*.fa'):
+        total_sequences = 0
+        infile = open(f, 'rU')
+        for line in infile:
+            if line.startswith('>'):
+                total_sequences += 1
+        print total_sequences
+        infile.seek(0)
+        file_counter = split_fasta(infile, outdir, jobs,
+                                   total_sequences, file_counter)
+        print file_counter
+        infile.close()
+
+if __name__ == "__main__":
+    main()
